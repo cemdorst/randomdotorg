4875419c6bd16ce519a62f9ae1f4d97c98c89060
Dorst <carlos.dorst@ceitec-sa.com>
2021-02-04 08:39:16 -0300

    Add unit tests and error handling

--- randomdotorg.go
+++ randomdotorg.go
@@ -3,6 +3,7 @@ package randomdotorg
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -17,36 +18,57 @@ var APIKey string
 //API base URL
 const apiurl string = "https://api.random.org/json-rpc/2/invoke"
 
+//Mininum sides on a dice
+const minsides = "2"
+const minresult = "1"
+
+//ErrDiceSides states minimum Dice sides
+var ErrDiceSides = errors.New("Dice must have more than" + minsides)
+
+//ErrJSON related to JSON operations
+var ErrJSON = errors.New("JSON Marshal/Unmarshal error")
+
+//ErrHTTP related to HTTP operations
+var ErrHTTP = errors.New("HTTP related error")
+
 //RollDice will throw (n) dice with (s) sides
-func RollDice(n string, s string) []int {
+func RollDice(n string, s string) (result []int) {
+	if s < minsides {
+		fmt.Println(ErrDiceSides)
+		return result
+	}
 
-	i := "1" //Dice sides go from i to s
-	params := []string{APIKey, n, i, s}
+	params := []string{APIKey, n, minresult, s}
 
-	req, _ := json.Marshal(jsonrpc2.Request{Method: "generateIntegers",
+	req, err := json.Marshal(jsonrpc2.Request{Method: "generateIntegers",
 		Params: params,
 		ID:     0,
 	})
+
+	if err != nil {
+		fmt.Println(ErrJSON.Error())
+	}
 
 	httpRes, err := http.Post(apiurl, "application/json",
 		bytes.NewReader(req))
 
 	if err != nil {
-		fmt.Printf("Error:%v", err.Error())
+		fmt.Printf(ErrHTTP.Error())
 	}
 
 	respBytes, err := ioutil.ReadAll(httpRes.Body)
 
 	if err != nil {
-		fmt.Printf("Error:%v", err.Error())
+		fmt.Printf(ErrHTTP.Error())
 	}
 
 	var diceRoll model.Dice
 
 	err = json.Unmarshal(respBytes, &diceRoll)
 	if err != nil {
-		fmt.Printf("Error:%v", err.Error())
+		fmt.Printf(ErrJSON.Error())
 	}
 
-	return diceRoll.Result.Random.Data
+	result = diceRoll.Result.Random.Data
+	return
 }
---
+++ randomdotorg_test.go
@@ -0,0 +1,29 @@
+package randomdotorg
+
+import "testing"
+
+func Init() {
+	APIKey = ""
+}
+
+func TestRollDice(t *testing.T) {
+	Init()
+	result := RollDice("4", "6")
+	if len(result) != 4 {
+		t.Errorf("RollDice result length should be 4, but returned: %d", len(result))
+	}
+	for dice := range result {
+		if dice > 6 {
+			t.Errorf("RollDice should be <= 6, but was: %d", result)
+		}
+	}
+}
+
+func TestRollDiceEmpty(t *testing.T) {
+	Init()
+	result := RollDice("2", "6")
+	if len(result) == 0 {
+		t.Errorf("RollDice result length should be 1, but returned: %d", len(result))
+	}
+
+}
